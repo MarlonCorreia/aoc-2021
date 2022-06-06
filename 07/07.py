60aac6086f0c214ab15bc9776f1b01fa4c8a73f3
MarlonCorreia <malilocsimas26@gmail.com>
2022-06-05 22:55:11 -0300

    Add day 07 part2 solution

--- 07/07.py
+++ 07/07.py
@@ -1,3 +1,10 @@
+from statistics import mean
+
+def _triang_sum(n):
+    number = 0
+    for i in range(1, n+1):
+        number += i
+    return number
 
 def part1(steps):
     l = []
@@ -9,8 +16,24 @@ def part1(steps):
     
     return min(l)
 
+def part2(steps):
+    m = int(mean(steps))
+    n = []
+    for i in range(m, m+2):
+        f = 0
+        for v in steps:
+            f += _triang_sum(abs(i-v))
+        n.append(f)
+
+    return min(n)
+
+
+
 with open('/home/marlinho/projects/aoc-2021/07/input.txt') as f:
     steps = [int(x) for x in f.read().split(',')]
 
     p1 = part1(steps)
+    p2 = part2(steps)
+    
     print(f"Part1 {p1}")
+    print(f"Part2 {p2}")
